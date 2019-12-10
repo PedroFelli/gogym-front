90ea5630eac13d42d01abe4dadf183761be71b2d
Pedro <pedrofellipeunai@gmail.com>
2019-12-10 09:00:40 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
+import Matriculas from '../pages/Matriculas';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/matriculas" component={Matriculas} isPrivate />
+    </Switch>
+  );
+}
