a52632cde71350a02467f4a2686361d2c771e0f2
Pedro <pedrofellipeunai@gmail.com>
2019-12-18 19:29:30 -0300

    finalizando o login

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
+import persistReducer from './persistReducers';
 import createStore from './createStore';
 
 import rootReducer from './modules/rootReducer';
@@ -13,8 +16,9 @@ const sagaMiddlewares = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddlewares];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persistReducer(rootReducer), middlewares);
+const persistor = persistStore(store);
 
 sagaMiddlewares.run(rootSaga);
 
-export default store;
+export { store, persistor };
--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -1,17 +1,14 @@
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  token: null,
-  signed: false,
-  loading: false,
+  profile: null,
 };
 
-export default function auth(state = INITIAL_STATE, aciton) {
+export default function user(state = INITIAL_STATE, aciton) {
   switch (aciton.type) {
     case '@auth/SIGN_IN_SUCESS':
       return produce(state, draft => {
-        draft.token = aciton.payload.token;
-        draft.signed = true;
+        draft.profile = aciton.payload.user;
       });
     default:
       return state;
